051860a0a9de133d75977f410ed12fc9e849d566
Jakub Jafra <jakub.jafra@gmail.com>
2018-09-30 13:57:35 +0200

    No-op web only plugins example usage

--- crna/src/components/Button.story.js
+++ crna/src/components/Button.story.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import {
     storiesOf,
+    fullScreenHostDecorator,
 } from 'react-native-hybrid-storybook';
 import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs/react';
 
 import Button from './Button';
 
 storiesOf('Button', module)
+    .addWithChapters('Story with chapters', {
+        subtitle: 'Story subtitle',
+        info: 'Story info test **with formatting** ;)',
+        chapters: [
+            {
+                title: 'Chapter title',
+                subtitle: 'Chapter subtitle',
+                info: `
+                    Let\'s render something!
+                    
+                    \`\`\`
+                    TEST
+                    \`\`\`
+                `,
+                sections: [
+                    {
+                        title: 'Section title',
+                        subtitle: 'Section subtitle',
+                        info: 'Section info',
+                        sectionFn: () => (
+                            <Button
+                                onClick={action('onClick')}
+                                title={text('title', 'Test button')}
+                            />
+                        ),
+                    }
+                ]
+            }
+        ]
+    });
+
+storiesOf('Button', module)
+    .addDecorator(fullScreenHostDecorator)
     .add('Example', () => (
         <Button
             onClick={action('onClick')}
