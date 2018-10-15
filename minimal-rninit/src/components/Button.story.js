8915595778f7d11aa7eefe5d2e290fa81bd432c3
Jakub Jafra <jakub.jafra@gmail.com>
2018-10-15 16:44:49 +0200

    Minimal rninit project integration

---
+++ minimal-rninit/src/components/Button.story.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    storiesOf,
+    fullScreenHostDecorator,
+} from 'react-native-hybrid-storybook';
+import Button from './Button';
+
+storiesOf('Button', module)
+    .addDecorator(fullScreenHostDecorator)
+    .add('Example', () => (
+        <Button title="Test button" />
+    ))
+    .add('Example 2', () => (
+        <Button title="Test button 2" />
+    ));
